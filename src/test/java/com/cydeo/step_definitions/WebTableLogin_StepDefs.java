dd8f9b08300a069e8e3ad21b5fea2f5990afc078
FriyatMelaki <friyath@gmail.com>
2022-10-28 21:18:05 -0500

    WebTableLogin_StepDefs.java WebTableLoginPage

--- src/test/java/com/cydeo/step_definitions/WebTableLogin_StepDefs.java
+++ src/test/java/com/cydeo/step_definitions/WebTableLogin_StepDefs.java
@@ -1,34 +1,58 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.WebTableLoginPage;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.Map;
 
 public class WebTableLogin_StepDefs {
 
+    WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
     @Given("user is on the login page of web table app")
     public void user_is_on_the_login_page_of_web_table_app() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+
     }
     @When("user enters username {string}")
     public void user_enters_username(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        webTableLoginPage.inputUsername.sendKeys(string);
+
     }
     @When("user enters password {string}")
     public void user_enters_password(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        webTableLoginPage.inputPassword.sendKeys(string);
+
     }
     @When("user clicks to login button")
     public void user_clicks_to_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        webTableLoginPage.loginButton.click();
+
     }
     @Then("user should see url contains orders")
     public void user_should_see_url_contains_orders() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("orders"));
+    }
+        @When("user enters username {string} password {string}")
+        public void user_enters_username_password(String username, String password) {
+           // webTableLoginPage.inputUsername.sendKeys(username);
+            //webTableLoginPage.inputPassword.sendKeys(password);
+           // webTableLoginPage.loginButton;
+
+            webTableLoginPage.login(username,password);
+
+    }
+
+    @When("user enters below credentials")
+    public void userEntersBelowCredentials(Map<String,String>credentials) {
+        //webTableLoginPage.inputUsername.sendKeys(credentials.get("username"));
+       // webTableLoginPage.inputPassword.sendKeys(credentials.get("password"));
+        //webTableLoginPage.loginButton.click();
+        webTableLoginPage.login(credentials.get("username"),credentials.get("password"));
+
     }
 }
--- src/test/resources/features/webtableLogin.feature
+++ src/test/resources/features/webtableLogin.feature
@@ -1,10 +1,23 @@
 Feature: user should be able to loin with valid credentials
 
-  @webtableApp
-  Scenario: Login scenario with 1 param
+  Background:
     Given user is on the login page of web table app
+
+
+  Scenario: Login scenario with 1 param
+
     When user enters username "Test"
     And user enters password "Tester"
     And user clicks to login button
     Then user should see url contains orders
 
+Scenario: Login scenario with 2 param
+  When user enters username "Test" password "Tester"
+  Then user should see url contains orders
+
+  @webtableApp
+  Scenario: Login scenario with data table
+    When user enters below credentials
+      | username     | Test   |
+      | password     | Tester |
+    Then user should see url contains orders
